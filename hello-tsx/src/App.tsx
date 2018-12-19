94340efd998f73ed5ccb6f3b5f5ea01724d5d769
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-19 22:37:16 +0100

    st

--- hello-tsx/src/App.tsx
+++ hello-tsx/src/App.tsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyClass from './components/MyClass';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-      
+        <MyClass name={'Laurynas'} age={20} password={'100nu'} isMarried={true} />
       </div>
     );
   }
--- hello-tsx/src/components/MyClass.tsx
+++ hello-tsx/src/components/MyClass.tsx
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 
-class MyClass extends Component {
+export interface MyClassProps {
+     name: string; 
+     age: number;
+     password?: string;
+     isMarried: boolean;
+    }
+
+class MyClass extends React.Component<MyClassProps> {
   render() {
     return (
       <div>
-
+         <h1>{this.props.name}</h1>
+         <h1>{this.props.age}</h1>
+         <h1>{this.props.password}</h1>
+         <h1>{this.props.isMarried} ---</h1>
       </div>
     );
   }
